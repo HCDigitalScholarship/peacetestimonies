49c4c5bb7a9b9c04c6751adcad59a2cd0bf4c20f
root <root@ds-omeka.haverford.edu>
2015-09-16 13:07:06 -0400

    adds menu to neatline page

--- plugins/Neatline/views/public/javascripts/fullscreen.js
+++ plugins/Neatline/views/public/javascripts/fullscreen.js
@@ -15,7 +15,9 @@ $(function() {
   body.css('margin', 0);
 
   exhibit.detach();
-  $('body').empty().append(exhibit);
+  $('body').empty()
+  body.append("<nav id='top-nav' role='navigation'>  <ul class='navigation'>    <li>   <a href='/peacetestimonies/about-project'>About</a>   <ul>   <li>  <a href='/peacetestimonies/acknowledgements'>Acknowledgements</a>   </li>   <li>  <a href='/peacetestimonies/curatorial'>Curatorial Statement</a>   </li>   </ul>    </li>    <li>   <a href='/peacetestimonies/homepage'>Networks of Annotations by Artist</a>   <ul>   <li>  <a href='/peacetestimonies/network-of-annotations-hughes'>Langston Hughes</a>   </li>   <li>  <a href='/peacetestimonies/network-of-annotationspicasso'>Pablo Picasso</a>   </li>   <li>  <a href='/peacetestimonies/network-of-annotations-rukeyser'>Muriel Rukeyser</a>   </li>   <li>  <a href='/peacetestimonies/network-of-annotations-woolf'>Virginia Woolf</a>   </li>   </ul>    </li>    <li>   <a href='/peacetestimonies/posters'>Posters</a>   <ul>   <li>  <a href='/peacetestimonies/art-as-a-way-of-spreading-ideas'>Art as a Way of Spreading Ideas</a>   </li>   <li>  <a href='/peacetestimonies/definitions-of-pacifism'>Definitions of Pacifism</a>   </li>   <li>  <a href='/peacetestimonies/feminism-virginia-woolf'>Feminism: Virginia Woolf</a>   </li>   <li>  <a href='/peacetestimonies/pacifism-woolf'>Pacifism: Virgina Woolf</a>   </li>   <li>  <a href='/peacetestimonies/poetry-and-peace-ruykeser'>Poetry and Peace: Ruykeser</a>   </li>   <li>  <a href='/peacetestimonies/positive-peace-the-quakers-in-spain'>Positive Peace: the Quakers in Spain</a>   </li>   <li>  <a href='/peacetestimonies/total-war-picasso'>Total War: Picasso</a>   </li>   </ul>    </li>    <li>   <a href='https://ds-omeka.haverford.edu/peacetestimonies/neatline/fullscreen/mindmap2'>Annotations Map</a>    </li></ul>   </nav>")
+  $('body').append(exhibit);
 
   var position = function() {
     map.css('height', $(window).height());
