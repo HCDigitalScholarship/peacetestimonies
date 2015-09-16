5949ff50c137baa9a758f6e61bb2987dffbad3c6
root <root@ds-omeka.haverford.edu>
2015-09-16 17:02:34 -0400

    moves files to the top and removes link to item in omeka records show

--- plugins/Neatline/views/shared/exhibits/item.php
+++ plugins/Neatline/views/shared/exhibits/item.php
@@ -9,18 +9,15 @@
 
 ?>
 
-<!-- Texts. -->
-<?php echo all_element_texts('item'); ?>
-
 <!-- Files. -->
 <?php if (metadata('item', 'has files')): ?>
-  <h3><?php echo __('Files'); ?></h3>
   <?php echo files_for_item(); ?>
 <?php endif; ?>
+
+
+<!-- Texts. -->
+<?php echo all_element_texts('item'); ?>
 
 <hr />
 
 <!-- Link. -->
-<?php echo link_to(
-  get_current_record('item'), 'show', 'View the item in Omeka'
-); ?>
