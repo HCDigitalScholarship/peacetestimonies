73685756dca6ba425977fd460a1a2d4aab96b745
root <root@ds-omeka.haverford.edu>
2015-10-04 21:33:35 -0400

    not sure what these do

--- themes/minimalist/common/footer.php
+++ themes/minimalist/common/footer.php
@@ -11,7 +11,7 @@
                 <?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = option('copyright')): ?>
                     <p><?php echo $copyright; ?></p>
                 <?php endif; ?>
-                <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+                <p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>'); ?></p>
             </div>
 
             <?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
@@ -19,6 +19,18 @@
         </footer>
 
     </div><!-- end wrap -->
+
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45185227-12', 'auto');
+  ga('send', 'pageview');
+
+</script>
+
 
     <script>
     
